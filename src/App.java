a701d5b9d5117cfaf9f0240338f43f1feeb4df5f
Tállisson Santos <105830434+tallison-santos@users.noreply.github.com>
2024-06-25 13:55:02 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("olá, mundo!!!!!");
+        System.out.println("olá, mundo!");
     }
 }
